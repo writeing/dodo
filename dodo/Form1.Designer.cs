f4d1abe35c53e98ac6b4db101f95cd284fc38e79
wxc <853976858@qq.com>
2019-03-26 19:13:37 +0800

    wxc:finish draw

--- dodo/Form1.Designer.cs
+++ dodo/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1166, 754);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1243, 744);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1253, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(215, 765);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1071, 754);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "生成";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1468, 789);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- dodo/Form1.cs
+++ dodo/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,93 @@ namespace dodo
 {
     public partial class Form1 : Form
     {
+        List<int> localxlist = new List<int>();
+        List<int> localylist = new List<int>();
+        List<int> roillist = new List<int>();
+
+        private Bitmap bmp;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //load image
+            pictureBox1.Image = Image.FromFile("dodo.jpg");
+        }
+        /// <summary>
+        /// test button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+                       
+        }
+
+        /// <summary>
+        /// 生成按钮事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int AllStar = 200;
+            Random rd = new Random(DateTime.Now.Second);
+            Bitmap pic1 = (Bitmap)pictureBox1.Image;            
+            for (int i = 0; i < AllStar; i++)
+            {
+                //W:1519 H:657
+                roillist.Add(rd.Next(1, 10));
+                localxlist.Add(rd.Next(pic1.Width));
+                localylist.Add(rd.Next(pic1.Height));
+            }
+            this.pictureBox1.Invalidate();
+        }
+        /// <summary>
+        /// 重新绘制界面
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            bmp = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height, e.Graphics);
+            var g = Graphics.FromImage(bmp);
+            g.Clear(Color.White);
+            Pen pen = new Pen(Color.Black,1);
+            for (int i = 0; i < localxlist.Count; i++)
+            {
+                g.FillEllipse(new SolidBrush(Color.Black), localxlist[i], localylist[i], roillist[i], roillist[i]);                
+                g.DrawEllipse(pen, localxlist[i], localylist[i], roillist[i], roillist[i]);
+            }
+            e.Graphics.DrawImage(bmp, 0, 0);
+        }
+    }
+    public class EarthInfo
+    {
+        //const define
+        public enum DIRECT_SPEED
+        {
+            SPEED_UP,
+            SPEED_DOWN,
+            SPEED_RIGHT,
+            SPEED_LEFT,
+            SPEED_UP_LEFT,
+            SPEED_UP_RIGHT,
+            SPEED_DOWN_LEFT,
+            SPEED_DOWN_RIGHT,
+            SPEED_STOP,          
+        }
+
+        //value define
+        public string name;
+        public int localx;
+        public int localy;
+        public int roil;
+        public int speed;
+        public int direct;
+        public List<string> oldName = new List<string>();
+
     }
 }
